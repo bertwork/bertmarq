4540ae259cedc5be666c5dddd1aeca56ee0fa2ff
bertwork <adeniros24@gmail.com>
2026-04-29 14:38:49 +0800

    feat: split prisma schema into multiple files and update auth config

--- apps/api/src/lib/auth.ts
+++ apps/api/src/lib/auth.ts
@@ -1,20 +1,21 @@
 import { betterAuth } from "better-auth"
 import { prismaAdapter } from "better-auth/adapters/prisma"
 import { prisma } from "./prisma"
-import { admin } from "better-auth/plugins"
+import { admin, username } from "better-auth/plugins"
 import { allowedOrigins } from "../config/cors"
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-  baseURL: process.env.BETTER_AUTH_URL,
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
+  plugins: [admin(), username()],
+  baseURL: process.env.BETTER_AUTH_URL,
+  secret: process.env.BETTER_AUTH_SECRET,
   trustedOrigins: allowedOrigins,
-  plugins: [admin()],
 })
